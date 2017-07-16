cb9e4fb4a9c3bdb68a05f06f9a1b8efe5bbe9169
Kerri Shotts <kerrishotts@gmail.com>
2017-07-16 17:34:23 -0500

    export render

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 import Component from "./src/Component.js";
 import createElement from "./src/createElement.js";
-import render from "./src/render.js";
 import cvtVNode2DOM from "./src/cvtVNode2DOM.js";
 import expandVNodeTree from "./src/expandVNodeTree.js";
 import diff from "./src/diff.js";
+import render from "./src/render.js";
 
 export {
     Component,
     createElement,
+    render,
     /* things that are supposed to be temporary for now */
     expandVNodeTree,
-    render,
     cvtVNode2DOM,
     diff,
 };
