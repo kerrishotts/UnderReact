8843ba77c2f047bfff8285c1c43b6de78017f07f
Kerri Shotts <kerrishotts@gmail.com>
2017-07-16 17:34:12 -0500

    docs; more lifecycle triggers

--- src/Component.js
+++ src/Component.js
@@ -1,5 +1,6 @@
 import diff from "./diff.js";
 import VNode from "./VNode.js";
+import expandVNodeTree from "./expandVNodeTree.js";
 
 export default class Component {
     constructor(props = {}, context = {}) {
@@ -33,32 +34,102 @@ export default class Component {
      * @memberof Component
      */
     setState(newState) {
+        if (this.state === newState) {
+            return;
+        }
+
+        this._dirty = true;
+
         let nextState = newState;
-        const prevState = Object.assign({}, this.state);
+        const prevState = Object.assign({}, this.state),
+            prevProps = Object.assign({}, this.props),
+            prevContext = Object.assign({}, this.context);
+
         if (typeof newState === "function") {
             nextState = newState(prevState, this.props);
         }
+
         this.state = Object.assign({}, this.state, nextState);
-        // TODO: trigger lifecycle events?
-        // TODO: trigger render pass
+
+        // trigger a render at the next frame
         window.requestAnimationFrame(() => {
-            const rendering = this.render();
-            diff(this._previousRender, rendering, this._domNode);
+            let rendering = this._previousRender;
+            if (this.shouldComponentUpdate(this.props, this.state, this.context)) {
+                this.componentWillUpdate(this.props, this.state, this.context);
+                rendering = expandVNodeTree(this.render(this.props, this.state, this.context), this.context);
+                this.componentDidUpdate(prevProps, prevState, prevContext);
+            }
+            rendering._component = this;
+            diff(this._previousRender, rendering, this._domNode, this.context);
             this._previousRender = rendering;
+            this._dirty = false;
         });
     }
 
+    /**
+     * Determine whether or not the component should update. If `false` is
+     * returned, the component may not be updated.
+     *
+     * @param {any} nextProps
+     * @param {any} nextState
+     * @param {any} nextContext
+     * @returns
+     * @memberof Component
+     */
     shouldComponentUpdate(nextProps, nextState, nextContext) {
         return this._dirty;
     }
 
+    /**
+     * Fires before the component is officially added to the DOM. (In our case,
+     * it's already there, but the component doesn't know that)
+     *
+     * @memberof Component
+     */
     componentWillMount() {}
+
+    /**
+     * Fires after the component is officially added to the DOM.
+     *
+     * @memberof Component
+     */
     componentDidMount() {}
 
+    /**
+     * Fires before a component is removed from the DOM.
+     *
+     * @memberof Component
+     */
     componentWillUnmount() {}
 
-    componentWillReceiveProps(nextProps, nextContext) {}
+    /**
+     * Fires when a component receives new props
+     *
+     * @param {any} nextProps
+     * @param {any} nextContext
+     * @memberof Component
+     */
+    componentWillReceiveProps(nextProps, nextContext) {
+        this.props = nextProps;
+    }
 
+    /**
+     * Called before the component is about to be rendered
+     *
+     * @param {any} nextProps
+     * @param {any} nextState
+     * @param {any} nextContext
+     * @memberof Component
+     */
     componentWillUpdate(nextProps, nextState, nextContext) {}
+
+    /**
+     * Called after the component has been rendered
+     *
+     * @param {any} prevProps
+     * @param {any} prevState
+     * @param {any} prevContext
+     * @memberof Component
+     */
     componentDidUpdate(prevProps, prevState, prevContext) {}
 }
